1f1d7b59d82988aa067a830ffa6976debf17a3f9
ruth <rsugiart@purdue.edu>
2024-10-20 13:48:12 -0400

    background generation

---
+++ graphics/background.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+width = 240
+height = 320
+name = 'background'
+
+# Initialize to zero
+arr = [ [0 for x in range(0,height)] for y in range(0,width) ]
+
+
+black = 0x0000
+white = 0xffff
+
+# Make a checkerboard with green boundaries
+for x in range(0, width):
+  for y in range(0, height):
+    arr[x][y] = black
+    # if x < border or x >= width-border:
+    #   arr[x][y] = green
+    #   continue
+    # if y < border or y >= height-border:
+    #   arr[x][y] = green
+    #   continue
+    # if (x // 20) % 2 == 0:
+    #   if (y // 20) % 2 == 0:
+    #     arr[x][y] = black
+    #   else:
+    #     arr[x][y] = white
+    # else:
+    #   if (y // 20) % 2 == 0:
+    #     arr[x][y] = white
+    #   else:
+    #     arr[x][y] = black
+
+out = open('./graphics/background.c','w')
+
+# Print out the header
+out.write("const struct {\n")
+out.write("  unsigned int width;\n")
+out.write("  unsigned int height;\n")
+out.write("  unsigned int bytes_per_pixel;\n")
+out.write("  unsigned char pixel_data[%d * %d * 2 + 1];\n" % (width,height))
+out.write("} %s = {\n" % name)
+out.write("  %d, %d, 2,\n" % (width,height))
+
+# Print out one row at a time from top to bottom
+for y in range(0, height):
+  out.write('  "')
+  for x in range(0, width):
+    # Print each 16-bit value in little-endian format
+    out.write('\\%03o\\%03o' % (arr[x][y]&0xff, arr[x][y]>>8))
+  out.write('"\n')
+
+out.write("};\n")
+
